6878c8125578f1977f854ae393bcdd82ccb1bc11
ferran <ferran.salazar@gmail.com>
2023-12-18 19:57:39 +0100

    Final code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import static org.example.FileManager.readCSV;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
